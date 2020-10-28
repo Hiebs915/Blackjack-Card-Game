0c0f599be1624a1820cf82a3631a07f77f96ea7a
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-28 07:11:42 -0500

    Update Blackjack Card Game.py

--- Blackjack Card Game.py
+++ Blackjack Card Game.py
@@ -19,7 +19,6 @@ class Card:
 #-----------------------------------------------------------------------------#
 
 class Deck:
-
     def __init__(self):
         self.deck = []  # start with an empty list
         for suit in suits:
@@ -29,7 +28,7 @@ class Deck:
     def __str__(self):
         deck_comp = ''  # start with an empty string
         for card in self.deck:
-            deck_comp += '\n '+card.__str__() # add each Card object's print string
+            deck_comp += '\n '+ card.__str__() # add each Card object's print string
         return 'The deck has:' + deck_comp
 
     def shuffle(self):
